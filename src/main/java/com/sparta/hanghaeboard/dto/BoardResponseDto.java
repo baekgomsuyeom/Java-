ed423fb7ae1b695d307490ea9aaabdedd04010cb
baekgomsuyeom <117441902+baekgomsuyeom@users.noreply.github.com>
2022-12-01 17:48:35 +0900

    Update BoardResponseDto.java

--- src/main/java/com/sparta/hanghaeboard/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/hanghaeboard/dto/BoardResponseDto.java
@@ -2,12 +2,12 @@ package com.sparta.hanghaeboard.dto;
 
 import com.sparta.hanghaeboard.entity.Board;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-
+@NoArgsConstructor
 
 public class BoardResponseDto {
     private String title;
@@ -17,17 +17,21 @@ public class BoardResponseDto {
     private LocalDateTime modifiedAt;
     private LocalDateTime createdAt;
     private Long id;
+    private String msg;
+    private int statusCode;
 
+    //생성자
     public BoardResponseDto(Board board) {
         this.title = board.getTitle();
         this.username = board.getUsername();
         this.contents = board.getContents();
-
         this.createdAt = board.getCreatedAt();
         this.modifiedAt = board.getModifiedAt();
         this.id = board.getId();
     }
+
+    public BoardResponseDto(String msg, int statusCode) {
+        this.msg = msg;
+        this.statusCode = statusCode;
+    }
 }
-
-
-
